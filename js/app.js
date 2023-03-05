951662953033ca3f2f1fe8b3879d4f85082694fa
mthtitumir <titumirgccc@gmail.com>
2023-03-05 08:48:01 +0600

    done

--- js/app.js
+++ js/app.js
@@ -4,14 +4,32 @@ const loadTemp = city => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayTemp(data));
-    console.log(url);
+    .then(data => displayTemp(data))
 }
+
+
 
 const displayTemp = data =>{
-    const temperature = document.getElementById('temp');
-    temperature.innerText = data.main.temp;
-
+    console.log(data.weather[0].main);
+    console.log( data.main.temp);
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('wether-condition', data.weather[0].main);
     
 }
-loadTemp('dhaka');
+
+const setInnerTextById = (id, text) =>{
+    const element = document.getElementById(id);
+    element.innerText = text;
+}
+
+
+
+document.getElementById('search-btn').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city
+    document.getElementById('city').innerText= city;
+    
+    loadTemp(city);
+})
+// loadTemp('dhaka');
